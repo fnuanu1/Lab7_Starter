73290f3047d1f58beac0a5c113f55b76d0430d60
fnuanu1 <150562704+fnuanu1@users.noreply.github.com>
2025-05-20 22:48:57 -0700

    all done/check again

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
+Team Members: Fnu Anu and Thanh Hoang
 
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+A: Among the other options, I choose the option that says, "Within a Github action that runs whenever code is pushed" because this is automated to whenever I push the code and try merging it to the main branch so we can test for our changes to be validated before deployment. This allows earlier caught of bugs, as mentioned in today's class, based on how you set a priority test list in github actions or pipeline.
 
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+A: No, I would not use end to end test to check if a function is returning the correct output because this can be tested easily through a jest test with expect `(__).toBe(__)` format.
+
+3) What is the difference between navigation and snapshot mode?
+The differences between navigation and snapshot mode is that navigation mode tests the webpage after it loads while snapshot mode simply tests the webpage in the state the website is in when that the snapshot mode tool is run for mostly accessibility issues found in that specific time only. Navigation mode also provided a lot of feedback on js, css and other minify or minimizations which snapshot mode did not really mentioned that. There is more detailed or general improvement feedback in navigation mode for general improvement in my opinion and more niche feedback in snapshot, as I observed in my experience.
+
+1) Name three things we could do to improve the CSE 110 shop site based on the Lighthouse results.
+2. Pre-portion the image sizes as when the images are bigger originally to being displayed smaller, this can increase the number of data and time a user uses to load the website on their device when we can size it already properly before using in our website.
+3. The following html tags at the beginning were missing to describe the language this html code is in, which will be english, which is for accessibility purposes or other languages respectively. This can be problematic because this assumes that the "page is in the default language that the user chose when setting up the screen reader" as said by the tool. I Viewport tag seems to avoid 300ms delay to user requests and adapt quickly to various devices with narrow screens. 
+So add the following accordingly:
+```<html lang="en">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+```
+1. Additionally, non-composited animations are any animation that triggers one of the earlier steps in the rendering pipeline, requiring more work to be done than necessary which is what our navigation mode seems to detect on Add to Cart buttons all over the website for 20 various items. Hence, practicing proper prevention to avoid non-composited animations will allow us to reduce jankiness and decrease CLS, which means a low-number equals to more stable website experiences versus a high CLS number means a much more janky experience.
 
 
 
--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -21,7 +21,7 @@ describe('Basic user flow for Website', () => {
   // Check to make sure that all 20 <product-item> elements have data in them
   // We use .skip() here because this test has a TODO that has not been completed yet.
   // Make sure to remove the .skip after you finish the TODO. 
-  it.skip('Make sure <product-item> elements are populated', async () => {
+  it('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -35,7 +35,7 @@ describe('Basic user flow for Website', () => {
       });
     });
 
-    console.log(`Checking product item 1/${prodItemsData.length}`);
+    console.log(`Checking product item 1/${prodItemsData.length}`);                                   //add more?
 
     // Make sure the title, price, and image are populated in the JSON
     firstValue = prodItemsData[0];
@@ -52,12 +52,125 @@ describe('Basic user flow for Website', () => {
       it checks every <product-item> it found
     * Remove the .skip from this it once you are finished writing this test.
     */
+      secondValue = prodItemsData[1];
+      if (secondValue.title.length == 0) { allArePopulated = false; }
+      if (secondValue.price.length == 0) { allArePopulated = false; }
+      if (secondValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      thirdValue = prodItemsData[2];
+      if (thirdValue.title.length == 0) { allArePopulated = false; }
+      if (thirdValue.price.length == 0) { allArePopulated = false; }
+      if (thirdValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      fourValue = prodItemsData[3];
+      if (fourValue.title.length == 0) { allArePopulated = false; }
+      if (fourValue.price.length == 0) { allArePopulated = false; }
+      if (fourValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      fiveValue = prodItemsData[4];
+      if (fiveValue.title.length == 0) { allArePopulated = false; }
+      if (fiveValue.price.length == 0) { allArePopulated = false; }
+      if (fiveValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      sixValue = prodItemsData[5];
+      if (sixValue.title.length == 0) { allArePopulated = false; }
+      if (sixValue.price.length == 0) { allArePopulated = false; }
+      if (sixValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      sevenValue = prodItemsData[6];
+      if (sevenValue.title.length == 0) { allArePopulated = false; }
+      if (sevenValue.price.length == 0) { allArePopulated = false; }
+      if (sevenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      eightValue = prodItemsData[7];
+      if (eightValue.title.length == 0) { allArePopulated = false; }
+      if (eightValue.price.length == 0) { allArePopulated = false; }
+      if (eightValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      nineValue = prodItemsData[8];
+      if (nineValue.title.length == 0) { allArePopulated = false; }
+      if (nineValue.price.length == 0) { allArePopulated = false; }
+      if (nineValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      tenValue = prodItemsData[9];
+      if (tenValue.title.length == 0) { allArePopulated = false; }
+      if (tenValue.price.length == 0) { allArePopulated = false; }
+      if (tenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      elevenValue = prodItemsData[10];
+      if (elevenValue.title.length == 0) { allArePopulated = false; }
+      if (elevenValue.price.length == 0) { allArePopulated = false; }
+      if (elevenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      twelveValue = prodItemsData[11];
+      if (twelveValue.title.length == 0) { allArePopulated = false; }
+      if (twelveValue.price.length == 0) { allArePopulated = false; }
+      if (twelveValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      thirteenValue = prodItemsData[12];
+      if (thirteenValue.title.length == 0) { allArePopulated = false; }
+      if (thirteenValue.price.length == 0) { allArePopulated = false; }
+      if (thirteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      fourteenValue = prodItemsData[13];
+      if (fourteenValue.title.length == 0) { allArePopulated = false; }
+      if (fourteenValue.price.length == 0) { allArePopulated = false; }
+      if (fourteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      fiveteenValue = prodItemsData[14];
+      if (fiveteenValue.title.length == 0) { allArePopulated = false; }
+      if (fiveteenValue.price.length == 0) { allArePopulated = false; }
+      if (fiveteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+  
+      sixteenValue = prodItemsData[15];
+      if (sixteenValue.title.length == 0) { allArePopulated = false; }
+      if (sixteenValue.price.length == 0) { allArePopulated = false; }
+      if (sixteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      seventeenValue = prodItemsData[16];
+      if (seventeenValue.title.length == 0) { allArePopulated = false; }
+      if (seventeenValue.price.length == 0) { allArePopulated = false; }
+      if (seventeenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      eighteenValue = prodItemsData[17];
+      if (eighteenValue.title.length == 0) { allArePopulated = false; }
+      if (eighteenValue.price.length == 0) { allArePopulated = false; }
+      if (eighteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      nineteenValue = prodItemsData[18];
+      if (nineteenValue.title.length == 0) { allArePopulated = false; }
+      if (nineteenValue.price.length == 0) { allArePopulated = false; }
+      if (nineteenValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
+
+      twentyValue = prodItemsData[19];
+      if (twentyValue.title.length == 0) { allArePopulated = false; }
+      if (twentyValue.price.length == 0) { allArePopulated = false; }
+      if (twentyValue.image.length == 0) { allArePopulated = false; }
+      expect(allArePopulated).toBe(true);
 
   }, 10000);
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
   // the button swaps to "Remove from Cart"
-  it.skip('Clicking the "Add to Cart" button should change button text', async () => {
+  it('Clicking the "Add to Cart" button should change button text', async () => {
     console.log('Checking the "Add to Cart" button...');
 
     /**
@@ -69,11 +182,24 @@ describe('Basic user flow for Website', () => {
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
+     // Query select all of the <product-item> elements
+    const textclick = await page.$eval('product-item', prodItems => {
+      const shadow1 = prodItems.shadowRoot.querySelector('button');
+     // const but1 = shadow1.querySelector('button');
+    //const text = shadow1.innerText.jsonValue;
+   // expect(text).toBe('Add to Cart');
+    shadow1.click();
+    const text1 = shadow1.innerText; 
+    shadow1.click();                                                             //why???? jsonValue not work?          
+    return text1;
+    });    
+    expect(textclick).toBe("Remove from Cart");
+
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
   // number in the top right has been correctly updated
-  it.skip('Checking number of items in cart on screen', async () => {
+  it('Checking number of items in cart on screen', async () => {
     console.log('Checking number of items in cart on screen...');
 
     /**
@@ -84,10 +210,24 @@ describe('Basic user flow for Website', () => {
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
+       //const cartNumber = await page.evaluate(() => { //evaluate helps run javascript in browser as a whole
+      // const products = await page.$$('product-item'); //individually into browser
+       // return products.length;
+       
+       const cartNumber = await page.evaluate(() => {
+        const products = document.getElementsByTagName('product-item');
+        for (let i= 0; i < products.length ; i++) {
+          const but = products[i].shadowRoot.querySelector('button');
+          but.click();
+        }
+       return document.getElementById("cart-count").innerText;
+       });
+      expect(cartNumber).toBe('20');
+
   }, 10000);
 
   // Check to make sure that after you reload the page it remembers all of the items in your cart
-  it.skip('Checking number of items in cart on screen after reload', async () => {
+  it('Checking number of items in cart on screen after reload', async () => {
     console.log('Checking number of items in cart on screen after reload...');
 
     /**
@@ -97,11 +237,28 @@ describe('Basic user flow for Website', () => {
      * Also check to make sure that #cart-count is still 20
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
+await page.reload();
+const cartNumber = await page.$$eval('product-item', products => {
+  let error = true; //no error
+  for (let i= 0; i < products.length ; i++) {
+    const but = products[i].shadowRoot.querySelector('button');
+    if(but.innerText != 'Remove from Cart'){
+       error = 'false-remove';
+      return error;
+    };
+  }
+ if (document.getElementById("cart-count").innerText != '20'){
+  error = 'false-nonumber'
+  return error;
+ }
+ return error;
+ });
+expect(cartNumber).toBe(true);
 
   }, 10000);
 
   // Check to make sure that the cart in localStorage is what you expect
-  it.skip('Checking the localStorage to make sure cart is correct', async () => {
+  it('Checking the localStorage to make sure cart is correct', async () => {
 
     /**
      **** TODO - STEP 5 **** 
@@ -109,12 +266,18 @@ describe('Basic user flow for Website', () => {
        '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
+       const localStorageData = await page.evaluate(() => {
+        return localStorage.getItem('cart'); //can aklso use parse I believe to get the whole array
+        //JSON.stringify(arr);
+      });
+
+      expect(localStorageData).toBe('[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]');
 
   });
 
   // Checking to make sure that if you remove all of the items from the cart that the cart
   // number in the top right of the screen is 0
-  it.skip('Checking number of items in cart on screen after removing from cart', async () => {
+  it('Checking number of items in cart on screen after removing from cart', async () => {
     console.log('Checking number of items in cart on screen...');
 
     /**
@@ -124,11 +287,28 @@ describe('Basic user flow for Website', () => {
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
+  //    const textclick = await page.$eval('product-item', prodItems => {
+  //   //const text = shadow1.innerText.jsonValue;
+  //  // expect(text).toBe('Add to Cart');
+  //   shadow1.click();
+  //   //const text1 = shadow1.innerText; 
+  //   //shadow1.click();                                                             //why???? jsonValue not work?          
+  //   //return text1;
+
+  const cartNumber = await page.$$eval('product-item', products => {
+    //let error = true; //no error
+    for (let i= 0; i < products.length ; i++) {
+      const but = products[i].shadowRoot.querySelector('button');
+      but.click();
+    }
+   return document.getElementById("cart-count").innerText;
+   });
+  expect(cartNumber).toBe('0');
   }, 10000);
 
   // Checking to make sure that it remembers us removing everything from the cart
   // after we refresh the page
-  it.skip('Checking number of items in cart on screen after reload', async () => {
+  it('Checking number of items in cart on screen after reload', async () => {
     console.log('Checking number of items in cart on screen after reload...');
 
     /**
@@ -138,12 +318,29 @@ describe('Basic user flow for Website', () => {
      * Also check to make sure that #cart-count is still 0
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
+    await page.reload();
+       const cartNumber = await page.$$eval('product-item', products => {
+        let error = true; //no error
+        for (let i= 0; i < products.length ; i++) {
+          const but = products[i].shadowRoot.querySelector('button');
+          if(but.innerText!="Add to Cart"){
+            error = false;
+            return error;
+          }
+        }
+       if(document.getElementById("cart-count").innerText != '0'){
+        error=false;
+        return error;
+       }
+       return error;
+       });
+      expect(cartNumber).toBe(true);
 
-  }, 10000);
+  }, 25000);
 
   // Checking to make sure that localStorage for the cart is as we'd expect for the
   // cart being empty
-  it.skip('Checking the localStorage to make sure cart is correct', async () => {
+  it('Checking the localStorage to make sure cart is correct', async () => {
     console.log('Checking the localStorage...');
 
     /**
@@ -151,6 +348,11 @@ describe('Basic user flow for Website', () => {
      * At this point he item 'cart' in localStorage should be '[]', check to make sure it is
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
+     const localStorageData = await page.evaluate(() => {
+      return localStorage.getItem('cart'); //can aklso use parse I believe to get the whole array
+      //JSON.stringify(arr);
+    });
 
+    expect(localStorageData).toBe('[]');
   });
 });
